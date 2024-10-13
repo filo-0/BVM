e2512f7ffd1f02e20f1f34c7a63e1a4f55a7a2f6
Filo.0 <filo.battisti@gmail.com>
2024-10-13 17:09:08 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// BytecodeVM.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <chrono>
+#include "bvm.hpp"
+
+int main()
+{
+	return BVM::RunTest();
+}
